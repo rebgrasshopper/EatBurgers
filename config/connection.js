b6b7529fb45f9596b7a473e8924b7d9192860dba
Plover Brown <rellwoos@gmail.com>
2020-09-28 17:08:14 -0700

    css styling

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,5 @@ const connection = mysql.createPool({
     database: dbConfig.DB,
 });
 
-// //activate connection
-// connection.connect(function(err){
-//     if (err) {
-//         return console.error("error connecting: " + err.stack);
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
-
 
 module.exports = connection;
